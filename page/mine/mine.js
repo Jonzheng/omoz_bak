3c51e8a37e8cb1eb9721aca8b3a3c377d05a5f3a
Jon <jonoz@qq.com>
2018-04-09 00:10:44 +0800

    mine -top

--- page/mine/mine.js
+++ page/mine/mine.js
@@ -1,13 +1,22 @@
 
+const app = getApp()
 Page({
 
   data: {
-      doo:"animate",
+
+      icon_coin:"../../image/coin.png",
+      icon_avatar:"",
+      nickname:"",
+      level:0,
       act:false
   },
   onclick:function(e){
+      var user_info = app.globalData["userInfo"]
+      console.log(user_info)
       this.setData({
-          act:!this.data.act
+          act:!this.data.act,
+          icon_avatar: user_info["avatarUrl"],
+          nickname: user_info["nickName"]
       })
   }
 })
